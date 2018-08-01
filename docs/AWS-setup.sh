065a97b25e2b3a407a337f375d46fa5db6bbd4ec
Pablo Barbera <pablobarbera@gmail.com>
2018-08-01 17:41:17 +0200

    extra materials day 4

--- docs/AWS-setup.sh
+++ docs/AWS-setup.sh
@@ -43,3 +43,51 @@ do
 	sudo chown ecpr$i -R /home/ecpr$i/
 done
 
+
+# copying files and changing permissions
+for i in 0{1..9} {10..40}
+do
+	cp /home/rstudio/code/05-advanced-scraping.Rmd /home/ecpr$i/code/
+	cp /home/rstudio/code/04-text-analysis.Rmd /home/ecpr$i/code/
+	cp /home/rstudio/code/challenge3.Rmd /home/ecpr$i/code/
+	cp /home/rstudio/data/trump-tweets.json /home/ecpr$i/data/	
+	sudo chown ecpr$i -R /home/ecpr$i/
+done	
+
+
+# solutions challenge 2, part 1
+for i in 0{1..9} {10..40}
+do
+	cp /home/rstudio/code/challenge2-solutions-part1.Rmd /home/ecpr$i/code/
+	cp /home/rstudio/code/challenge2-solutions-part1.html /home/ecpr$i/code/
+	sudo chown ecpr$i -R /home/ecpr$i/
+done
+
+
+# solutions challenge 2, full
+for i in 0{1..9} {10..40}
+do
+	cp /home/rstudio/code/challenge2-solutions.Rmd /home/ecpr$i/code/
+	cp /home/rstudio/code/challenge2-solutions.html /home/ecpr$i/code/
+	sudo chown ecpr$i -R /home/ecpr$i/
+done
+
+
+# materials day 3
+for i in 0{1..9} {10..40}
+do
+	cp /home/rstudio/code/06-newspaper-articles.Rmd /home/ecpr$i/code/
+	cp /home/rstudio/code/07-nytimes-api.Rmd /home/ecpr$i/code/
+	cp /home/rstudio/code/08-clarifai-api.Rmd /home/ecpr$i/code/
+	cp /home/rstudio/code/challenge4.Rmd /home/ecpr$i/code/
+	cp /home/rstudio/code/challenge5.Rmd /home/ecpr$i/code/
+	sudo chown ecpr$i -R /home/ecpr$i/
+done
+
+# solutions challenge 3
+for i in 0{1..9} {10..40}
+do
+	cp /home/rstudio/code/challenge3-solutions.Rmd /home/ecpr$i/code/
+	cp /home/rstudio/code/challenge3-solutions.html /home/ecpr$i/code/
+	sudo chown ecpr$i -R /home/ecpr$i/
+done
